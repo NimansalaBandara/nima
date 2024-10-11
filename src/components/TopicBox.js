cbc90c9c1111d4912db2ae7533260bda2f579938
NimansalaBandara <nimansaladamayanthibandara@gmail.com>
2024-10-11 21:43:21 +0530

    Update TopicBox.js

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './topicbox.css'
 
 export default function TopicBox() {
+  const topic = "my favourite food is kottu";
+  const item = "and rice";
   return (
     <div>
       <div className="topicBox">
-        <samp className='text'>This is the topic</samp>
+        <samp className='text'>{topic}{item}</samp>
       </div>
     </div>
   )
