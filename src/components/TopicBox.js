4fd63c300a66715d7d7bcec4a4629c257cfd51a6
NimansalaBandara <nimansaladamayanthibandara@gmail.com>
2024-10-12 13:20:38 +0530

    Update TopicBox.js

--- src/components/TopicBox.js
+++ src/components/TopicBox.js
@@ -3,7 +3,7 @@ import './topicbox.css'
 
 export default function TopicBox() {
   const topic = "my favourite food is kottu";
-  const item = "and rice";
+  const item = " and rice";
   return (
     <div>
       <div className="topicBox">
